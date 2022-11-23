74ce13867367de6d279102bca2e9e7a85498c64d
Gilvan Fernandes <gilvan@heavycodes.com>
2022-11-23 20:45:08 -0300

    UPDATE

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -7,7 +7,7 @@ const int LEDYELLOW = 10;
 const int LEDGREEN = 11;
 const int LEDRED = 12;
 
-int ldr = A0;//Atribui A0 a variável ldr
+int ldr = A1;//Atribui A0 a variável ldr
 int valorldr = 0;
 
 void setup() {
@@ -26,31 +26,35 @@ void loop() {
   valorldr    = analogRead(ldr);
   ADClido     = analogRead(LM35);
   temperatura = ADClido * 0.1075268817204301;
-  
-  Serial.print("Temperatura = ");
-  Serial.println(temperatura);
-  
+
 
   // LDR
   if(valorldr >= 900){
+    Serial.print("STATUS - SEM PRODUTO");  
     Serial.print("STATUS - SEM PRODUTO");
-    Serial.println(valorldr);
-  
-    digitalWrite(LEDYELLOW, HIGH);
-  }else{
-    Serial.print("STATUS - COM PRODUTO");
-    Serial.println(valorldr);
-  
-    digitalWrite(LEDYELLOW, LOW);
-  }
 
-  // TEMP.
-  if(temperatura > 27){ // setei como 30ºC
-    digitalWrite(LEDRED, HIGH);
-    digitalWrite(LEDGREEN, LOW);
-  }else{
+    digitalWrite(LEDYELLOW, HIGH);
     digitalWrite(LEDRED, LOW);
-    digitalWrite(LEDGREEN, HIGH);
+    digitalWrite(LEDGREEN, LOW);
+
+  }else{
+  
+    Serial.print("STATUS - COM PRODUTO");
+    
+    Serial.print("Temperatura = ");
+    Serial.println(temperatura);
+    
+    digitalWrite(LEDYELLOW, LOW);
+
+    // TEMP.
+    if(temperatura > 27){ // setei como 30ºC
+      digitalWrite(LEDRED, HIGH);
+      digitalWrite(LEDGREEN, LOW);
+    }else{
+      digitalWrite(LEDRED, LOW);
+      digitalWrite(LEDGREEN, HIGH);
+    }
+    
   }
   
   delay(1000);
